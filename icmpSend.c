135474bfd3f093064507a2beec1d9fcdde557e38
kristenkozmary <kristenkozmary@yahoo.com>
2016-11-12 21:07:57 -0600

    so many problems

--- icmpSend.c
+++ icmpSend.c
@@ -10,24 +10,24 @@ struct icmpPkt {
 };
 
 
-syscall icmpSend(struct ethergram *pkt, uchar type, uchar code, uint datalen,struct netaddr *src, struct netaddr *dst) {
+syscall icmpSend(struct ethergram *pkt, uchar type, uchar code, int datalen,struct netaddr *src, struct netaddr *dst) {
   struct ipgram *ip = NULL; //need the payload of ipgram to set as icmp
   //struct ethergram *egram = NULL; //not sure if we need this
   uchar *buf;
   uchar srcMac[ETH_ADDR_LEN];
-  
+  int i;
   buf = (uchar *)malloc(PKTSZ);
   
   control(ETH0, ETH_CTRL_GET_MAC, (ulong) srcMac, 0);
   
-  pkt->src = srcMac;
+  pkt->src = *srcMac; //incompatible types when assigning to type ‘uchar[6]’ from type ‘uchar *’
   
   for(i = 0; i < ETH_ADDR_LEN; i++)
   {
     pkt->dst[i] = 0XFF;
   }
   pkt->type = htons(ETYPE_IPv4);
-  pkt->data = ip;
+  pkt->data = ip; //incompatible types when assigning to type ‘char[1]’ from type ‘struct ipgram *’
   
   ip->ver_ihl = (uchar)(IPv4_VERSION << 4);
   ip->ver_ihl += IPv4_HDR_LEN; //may need IPv4_MAX_HDRLEN
@@ -37,12 +37,12 @@ syscall icmpSend(struct ethergram *pkt, uchar type, uchar code, uint datalen,str
   ip->flags_froff = 0;
   ip->ttl = IPv4_TTL;
   ip->proto = IPv4_PROTO_ICMP;
-  if (NULL == src->type) {
+  if (NULL == src->type) {    //dereferencing pointer to incomplete type
     memcpy(ip->src, pkt->src, IPv4_ADDR_LEN);
   } else {
-    memcpy(ip->src, src->addr, IPv4_ADDR_LEN);
+    memcpy(ip->src, src->addr, IPv4_ADDR_LEN);   //dereferencing pointer to incomplete type
   }
-  memcpy(ip->dst, dst->addr, IPv4_ADDR_LEN);
+  memcpy(ip->dst, dst->addr, IPv4_ADDR_LEN);      //dereferencing pointer to incomplete type
   
  // ip->chksum = 0;
  // ip->chksum = checksum((uchar *)ip, IPv4_HDR_LEN);
@@ -54,9 +54,9 @@ syscall icmpSend(struct ethergram *pkt, uchar type, uchar code, uint datalen,str
   icmp->type = type;
   icmp->code = code;
   icmp->chksum = 0;
-  icmp->chksum = netChksum((uchar *)icmp, datalen + ICMP_HEADER_LEN);
+  icmp->chksum = netChksum((uchar *)icmp, datalen + ICMP_HEADER_LEN);  //implicit declaration of function ‘netChksum’
   
-  ip->opts = icmp;
+  ip->opts = icmp;    //incompatible types when assigning to type ‘uchar[1]’ from type ‘struct icmpPkt *’
   
   icmp->data = 3;
   
@@ -78,6 +78,6 @@ syscall icmpSend(struct ethergram *pkt, uchar type, uchar code, uint datalen,str
   free(buf);
   
   
-  
+  return OK;
   
 }
